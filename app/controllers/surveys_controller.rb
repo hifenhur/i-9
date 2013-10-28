8ffa80b2e02de434d8d537d551766d1ee7530b44
Ben-Hur B. Oliveira <benhur@engenhariadesoftware.inf.br>
2013-10-28 00:40:49 -0200

    versao escolher

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -9,7 +9,11 @@ class SurveysController < ApplicationController
   # GET /surveys/1
   def show
     @survey_version = @survey.survey_versions.build
-    @last_survey_version = @survey.survey_versions.order('created_at desc').first
+    @last_survey_version = if params[:survey_version]
+      SurveyVersion.find(params[:survey_version])
+    else
+      @survey.survey_versions.order('created_at desc').first
+    end
   end
 
   # GET /surveys/new
