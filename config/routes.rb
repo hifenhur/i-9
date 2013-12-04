db951890ff04062d89213486f6d2d1d08561e7ad
Ben-Hur B. Oliveira <benhur@engenhariadesoftware.inf.br>
2013-12-03 23:33:10 -0200

    tudo

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Inove::Application.routes.draw do
-  Mercury::Engine.routes
+  
   resources :estudos do
     resources :documents
     
   end
 
   resources :surveys
-  
+
   get "home/index"
   resources :survey_versions
 
