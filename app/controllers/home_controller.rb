c5ef1877e755003f90d82419fcb8f366357db0b9
Ben-Hur B. Oliveira <benhur@engenhariadesoftware.inf.br>
2013-12-19 23:00:08 -0200

    removendo scaffolds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,6 @@ class HomeController < ApplicationController
 	skip_before_filter :authenticate_user!, only:[:index]
   def index
   	@documents = Document.all.limit(5)
+  	@map = Map.order('created_at desc').limit(1).take
   end
 end
